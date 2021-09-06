8ad75d6f0ed92ad73d583ff617ccd86027a69dbe
Alan Chen <chenalan2002@gmail.com>
2021-09-06 01:12:53 -0400

    improved joystick movement

--- Laser_Turret/Laser_Turret.ino
+++ Laser_Turret/Laser_Turret.ino
@@ -1,73 +1,116 @@
 #include <Servo.h>
-Servo servoX;
-Servo servoY;
 
 const int laserPin = 7;
 const int joystickXPin = A0;
 const int joystickYPin = A1;
-const int joystickClickPin = 2;
+const int joystickClickPin = A2;
+const int servoXPin = 9;
+const int servoYPin = 10;
 
 class Turret{
-  private:
+  public:
     Servo servoX;
-    Servo servoY;    
-
-    turret(){
-      servoX.attach(9);
-      servoY.attach(10);
-
-      pinMode(laserPin, OUTPUT);
-      }
-      
-  public:
-    virtual void move(servoXPos, servoYPos){
-      servoX.write(servoXPos);
-      servoY.write(servoYPos);
-    }
-}
-
-class LaserTurret : public Turret{
-  public:
-
+    Servo servoY;
+    
     int joystickXValue;
     int joystickYValue;
     int joystickClickValue;
-    
-    LaserTurret(){
+
+    int joystickXAngle = 90;
+    int joystickYAngle = 90;
+
+    bool calibrated = false;
+
+    int boundingAnglesFound = 0;
+    //[left, right, top, bottom]
+    int boundingAngles[4];
+
+    Turret(){
+      servoX.attach(servoXPin);
+      servoY.attach(servoYPin);
+
       pinMode(laserPin, OUTPUT);
       pinMode(joystickXPin, INPUT);
       pinMode(joystickYPin, INPUT);
       pinMode(joystickClickPin, INPUT);
       }
 
-    void flashLaser(){
-      if(joystickClickPin == 1):
-      //todo
-
-    void move override();
+    void joystick_move(){
       joystickXValue = analogRead(joystickXPin);
       joystickYValue = analogRead(joystickYPin);
+      
+      if (joystickXValue > 900)
+        joystickXAngle += 2;
+      else if (joystickXValue < 200)
+        joystickXAngle -= 2;
+      if (joystickYValue > 900)
+        joystickYAngle += 2;
+      else if (joystickYValue < 200)
+        joystickYAngle -= 2;
 
-      servoX.write(map(joystickXValue, 0, 1023, 0, 179));
-      servoY.write(map(joystickYValue, 0, 1023, 0, 179));
+      if (joystickYAngle > 180)
+        joystickYAngle = 180;
+      else if (joystickYAngle < 0)
+        joystickYAngle = 0;
+      if (joystickXAngle > 180)
+        joystickXAngle = 180;
+      else if (joystickXAngle < 0)
+        joystickXAngle = 0;
+        
+      servoX.write(joystickXAngle);
+      servoY.write(joystickYAngle);
     }
-}
+
+    void calibrate_boundaries(){
+      if (analogRead(joystickClickPin) < 10)
+      {
+        if (boundingAnglesFound < 3){
+          boundingAngles[boundingAnglesFound] = joystickXValue;
+          boundingAnglesFound++;
+        }
+        else
+        {
+          boundingAngles[boundingAnglesFound] = joystickYValue;
+          boundingAnglesFound++;
+          if (boundingAnglesFound > 3)
+            calibrated = true;
+        }
+        flash_laser();
+      }
+    }
+
+    void flash_laser (){
+      digitalWrite(laserPin, LOW);
+      delay(100);
+      digitalWrite(laserPin, HIGH);
+      delay(100);
+      digitalWrite(laserPin, LOW);
+      delay(100);
+      digitalWrite(laserPin, HIGH);
+      delay(100);
+      digitalWrite(laserPin, LOW);
+      delay(100);
+      digitalWrite(laserPin, HIGH);
+      delay(100);
+    }
+};
+
+Turret* turret;
 
 void setup() {
-  Turret turret;
+  turret = new Turret();
   digitalWrite(laserPin, HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop() {
- 
-  joystickXValue = analogRead(joystickXPin);
-  joystickYValue = analogRead(joystickYPin);
-
-  servoXPos = map(joystickXValue, 0, 1023, 0, 180);
-  servoYPos = map(joystickYValue, 0, 1023, 0, 180);
-
-  servoX.write(servoXPos);
-  servoY.write(servoYPos);
+  turret->joystick_move();
+  if (turret->calibrated == false)
+    turret->calibrate_boundaries();
+  //if(Serial.available()){
+  //      input = Serial.read();
+  // }
 
   delay(100);
 }
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import cv2
 import time
 import numpy as np
+import serial
 from background_subtractor import BackgroundSubtractor
 
 PROCESSING_SCALE = 1
 
 background_subtractor = BackgroundSubtractor(PROCESSING_SCALE)
+arduino = serial.Serial(port='COM3', baudraet= 9600, timeout=.1)
 
 while True:
 
     background_subtractor.update()
+    
     if cv2.waitKey(1) in [ord('q'), ord('Q')]:
         break
 
